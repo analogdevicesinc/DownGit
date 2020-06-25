bfde90398856ad0669ba2bbd918da3a7d896628b
Robin Getz <robin.getz@analog.com>
2020-06-25 11:25:06 -0400

    Make this only work with github.com/analogdevicesinc
    Some jackhole started using upstream DownGit to distirbute malware, so
    we have to run this ourselves.
    
    See:
    https://github.com/MinhasKamal/DownGit/issues/31
    
    Signed-off-by: Robin Getz <robin.getz@analog.com>

--- app/home/home.js
+++ app/home/home.js
@@ -30,9 +30,9 @@ homeModule.config([
                     $scope.downloadedFiles = {val: 0};
                     $scope.totalFiles = {val: 0};
 
-                    var templateUrl = "https?://github.com/.+/.+";
+                    var templateUrl = "https?://github.com/analogdevicesinc/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://minhaskamal.github.io/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://analogdevicesinc.github.io/DownGit/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
